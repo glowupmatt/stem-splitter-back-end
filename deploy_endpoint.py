d92b8cec3fba140c67dc6d990405eb5b5c9fcdc8
Matthew Nicholson <thematthewnicholson@gmail.com>
2025-03-29 20:32:32 -0700

    add model packaging and deployment scripts; update requirements

--- deploy_endpoint.py
+++ deploy_endpoint.py
@@ -7,7 +7,23 @@ from sagemaker.predictor import Predictor
 
 def deploy_endpoint() -> Predictor:
     sagemaker.Session()
-    role = ""
+    model = PyTorchModel(
+        role = "arn:aws:iam::593793052722:role/splitter-saas-role",
+        model_uri = "s3://splitter-saas/inference/model.tar.gz",
+        framework_version="2.5.1",
+        py_version="py311",
+        entry_point="inference.py",
+        source_dir=".",
+        name="splitter-saas-endpoint"
+    )
+    
+    predictor = model.deploy(
+        initial_instance_count=1,
+        instance_type="ml.g5.xlarge",
+        endpoint_name="splitter-saas-endpoint",
+    )
+    
+    
     
     
 if __name__ == "__main__": 
